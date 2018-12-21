35b73b88e5cf7443197b22cd920e71de88d84c44
Knut Hühne <huehne@htw-berlin.de>
2018-12-21 10:17:42 +0100

    add subtract method to Calculator

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -8,6 +8,11 @@ public class CalculatorTest {
     }
 
     @org.junit.Test
+    public void subtract(){
+        assertEquals(Calculator.subtract(2,1), 1);
+    }
+
+    @org.junit.Test
      public void addBigNumbers(){
         assertEquals(Calculator.add(1_000_000,2_000_000), 3_000_000);
     }
